1c86db0f8caa0584f2ebdf2fe47a2da82144b084
akemalsaglam <alikemalsaglam90@gmail.com>
2020-01-25 19:57:21 +0300

    add arffreader.

---
+++ src/custom/LineStringReader.java
@@ -0,0 +1,38 @@
+package custom;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class LineStringReader {
+    private String lineStringLine;
+    private List<Landmark> landmarks;
+
+    public LineStringReader(String lineStringLine) {
+        this.lineStringLine = lineStringLine;
+        landmarks = new ArrayList<>();
+    }
+
+    public String getLineStringLine() {
+        return lineStringLine;
+    }
+
+    public void setLineStringLine(String lineStringLine) {
+        this.lineStringLine = lineStringLine;
+    }
+
+    public List<Landmark> getLandmarks() {
+        return this.landmarks;
+    }
+
+    public void parse() {
+        String coordinatesString = this.lineStringLine.substring(12, this.lineStringLine.length() - 1);
+        String[] coordinates = coordinatesString.split(",");
+        Arrays.stream(coordinates).forEach(coordinate->{
+            Landmark landmark = new Landmark();
+            landmark.setY(Double.parseDouble(coordinate.trim().split(" ")[1].trim()));
+            landmark.setX(Double.parseDouble(coordinate.trim().split(" ")[0].trim()));
+            this.landmarks.add(landmark);
+        });
+    }
+}
